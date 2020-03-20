e03e283bea000b998af084b870e118760f5462ad
Steve Johnson <steve@abstractcoder.com>
2020-03-20 11:59:15 -0500

    Created a basic UI

--- config.rb
+++ config.rb
@@ -31,11 +31,56 @@ page '/*.txt', layout: false
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+require "csv"
+require "ostruct"
+
+helpers do
+  def clinic_data
+    @clinic_data ||= CSV.parse(File.read("data/clinic_data.csv"), headers: true)
+  end
+
+  def services
+    @services ||= create_hash_for_key(clinic_data, ["Clinical Services", "Routine Services"])
+  end
+
+  def languages
+    @languages ||= create_hash_for_key(clinic_data, "Language")
+  end
+
+  def bus_routes
+    @bus_routes ||= create_hash_for_key(clinic_data, "Bus Route")
+  end
+
+  def populations_served
+    @populations_served ||= create_hash_for_key(clinic_data, ["Payer Population Served", "Special Populations Served"])
+  end
+
+  def sort_hash(hash)
+    hash.sort_by{|service, ids| -ids.count}
+  end
+
+  def p(str)
+    (str || "").split(",").map(&:strip).join(", ")
+  end
+
+  def pp(str)
+    str.split(",").map(&:strip).join("<br>").html_safe
+  end
+
+  def create_hash_for_key(data, keys)
+    hash = Hash.new{|h,k| h[k] = []}
+
+    data.each_with_index do |row, index|
+      [keys].flatten.each do |key|
+        (row[key] || "").split(",").map(&:strip).compact.each do |service|
+          hash[service].push(index)
+        end
+      end
+    end
+
+    hash
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
